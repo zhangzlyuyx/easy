039eda84019d2f839edde5c24238c73c69ba7bdc
zhangzlyuyx <zhangzlyuyx@gmail.com>
2020-07-20 16:44:05 +0800

    PageResult 增加内存分页

--- easy-mybatis/src/main/java/com/zhangzlyuyx/easy/mybatis/PageResult.java
+++ easy-mybatis/src/main/java/com/zhangzlyuyx/easy/mybatis/PageResult.java
@@ -3,6 +3,8 @@ package com.zhangzlyuyx.easy.mybatis;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.zhangzlyuyx.easy.core.util.ConvertUtils;
+
 /**
  * 分页结果
  * @author zhangzlyuyx
@@ -100,5 +102,54 @@ public class PageResult<T> implements IPageResult<T> {
 	 */
 	public PageResult(List<T> rows) {
 		this.rows = rows;
+	}
+	
+	/**
+	 * 内存分页
+	 * @param rows 记录列表
+	 * @param pageNo 页码
+	 * @param pageSize 每页记录数
+	 * @return
+	 */
+	public static <E> PageResult<E> getPageResult(List<E> rows, Integer pageNo, Integer pageSize){
+		if(rows == null || rows.size() == 0) {
+			return new PageResult<>();
+		}
+		if(pageNo == null || pageNo.intValue() < 1) {
+			pageNo = 1;
+		}
+		if(pageSize == null || pageSize.intValue() < 1) {
+			pageSize = 1;
+		}
+		PageResult<E> result = new PageResult<>();
+		result.setPageNo(pageNo);
+		result.setPageSize(pageSize);
+		result.setTotal(ConvertUtils.toLong(rows.size(), 0L));
+		
+		//计算分页数
+		Integer pageCount = (rows.size() % pageSize == 0) ? (rows.size() / pageSize) : (rows.size() / pageSize + 1);
+		result.setPages(pageCount);
+		
+		//开始索引
+		int fromIndex = 0;
+		//结束索引
+        int toIndex = 0;
+ 
+        if(pageNo > pageCount){
+        	pageNo = pageCount;
+        }
+        if (!pageNo.equals(pageCount)) {
+            fromIndex = (pageNo - 1) * pageSize;
+            toIndex = fromIndex + pageSize;
+        } else {
+            fromIndex = (pageNo - 1) * pageSize;
+            toIndex = rows.size();
+        }
+        //截取列表
+        List<E> list = rows.subList(fromIndex, toIndex);
+		
+        result.setRows(list);
+		
+		return result;
 	}
 }
